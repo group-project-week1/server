f473a100ae85dea2009f3045d79e5763031f9897
nicholasbilly <nicholasbilly94@gmail.com>
2019-08-08 21:49:57 +0700

    tambah weather api

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express()
 const cors = require('cors')
 const PORT = 3000
 const unsplashRouter = require('./routes/unsplashRouter')
+const weatherRouter = require('./routes/weatherRouter')
 
 app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.use('/', unsplashRouter)
+app.use('/images', unsplashRouter)
+app.use('/weathers', weatherRouter)
 
 app.use((err, req, res, next) => {
     console.log(err.message)
--- routes/unsplashRouter.js
+++ routes/unsplashRouter.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 const unsplashController = require('../controllers/unsplashController')
 
-router.get('/search/:query', unsplashController.getImage)
+router.get('/:query', unsplashController.getImage)
 
 module.exports = router
